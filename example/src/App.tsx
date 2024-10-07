d32dbb416727cb9a4da71d53a314ed1f30a4107d
Joondong <kwonjoondong@gmail.com>
2024-10-07 14:11:37 +0900

    fix: separate example1

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,44 +1,6 @@
 import React from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
-import { observe } from 'react-native-observable-list';
-import TrackableItem from './TrackableItem';
-
-const ObservableFlatList = observe(FlatList);
-
-const outerData = Array.from({ length: 300 }).map(() => ({}));
-const innerData = Array.from({ length: 20 }).map(() => ({}));
+import Example1 from './Example1';
 
 export default function App() {
-  return (
-    <ObservableFlatList
-      style={styles.container}
-      data={outerData}
-      ListHeaderComponent={<View style={styles.header} />}
-      renderItem={({ index: outerIndex }) => {
-        if (outerIndex % 10) return <TrackableItem label={`${outerIndex}`} />;
-        return (
-          <ObservableFlatList
-            key={outerIndex}
-            data={innerData}
-            horizontal
-            renderItem={({ index: innerIndex }) => {
-              return (
-                <TrackableItem
-                  width={100}
-                  label={`${outerIndex}-${innerIndex}`}
-                />
-              );
-            }}
-          />
-        );
-      }}
-    />
-  );
+  return <Example1 />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  header: { height: 200, backgroundColor: 'green' },
-});
---
+++ example/src/Example1.tsx
@@ -0,0 +1,45 @@
+import { FlatList, StyleSheet, View } from 'react-native';
+import { observe } from 'react-native-observable-list';
+import TrackableItem from './TrackableItem';
+
+const ObservableFlatList = observe(FlatList);
+
+const outerData = Array.from({ length: 300 }).map(() => ({}));
+const innerData = Array.from({ length: 20 }).map(() => ({}));
+
+const Example1 = () => {
+  return (
+    <ObservableFlatList
+      style={styles.container}
+      data={outerData}
+      ListHeaderComponent={<View style={styles.header} />}
+      renderItem={({ index: outerIndex }) => {
+        if (outerIndex % 10) return <TrackableItem label={`${outerIndex}`} />;
+        return (
+          <ObservableFlatList
+            key={outerIndex}
+            data={innerData}
+            horizontal
+            renderItem={({ index: innerIndex }) => {
+              return (
+                <TrackableItem
+                  width={100}
+                  label={`${outerIndex}-${innerIndex}`}
+                />
+              );
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: { height: 200, backgroundColor: 'green' },
+});
+
+export default Example1;
