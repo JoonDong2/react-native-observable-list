06668eca44cbb95aa252d0b777f3104481ac3cb7
Joondong <kwonjoondong@gmail.com>
2024-10-08 13:00:42 +0900

    Revert "feat: example3"
    This reverts commit 3fdee116be6e5d2870c0699e1ce3d92dcb664184.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Example1 from './Example1';
 import Example2 from './Example2';
-import Example3 from './Example3';
 
 const EXAMPLE = 1;
 
@@ -11,9 +10,6 @@ export default function App() {
       return <Example1 />;
     case 2:
       return <Example2 />;
-    case 3:
-      return <Example3 />;
     default:
-      return null;
   }
 }
