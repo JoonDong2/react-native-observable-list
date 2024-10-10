86747f8d597615160fb5e86f7309999ef432ab1a
Joondong <kwonjoondong@gmail.com>
2024-10-10 18:37:24 +0900

    feat: add $$enabled

--- src/observe.tsx
+++ src/observe.tsx
@@ -10,6 +10,10 @@ import { type ViewToken } from 'react-native';
 type Clean = () => void;
 type Callback = () => Clean | undefined | void;
 
+const ConfigurationContext = createContext<{ enabled?: boolean }>({
+  enabled: true,
+});
+
 const CallbacksContext = createContext<{
   addCallback: (key: any, callback: Callback) => void;
   removeCallback: (key: any, clean: Clean) => void;
@@ -47,7 +51,7 @@ export const useInViewPort = (callback: Callback, deps?: any[]) => {
 
 export function observe<L extends React.ComponentType<any>>(List: L) {
   return React.forwardRef<L, any>(function (
-    { onViewableItemsChanged, keyExtractor, renderItem, ...props },
+    { onViewableItemsChanged, keyExtractor, renderItem, $$enabled, ...props },
     ref
   ) {
     const { isInViewPort, key } = useContext(ItemContext);
@@ -118,8 +122,17 @@ export function observe<L extends React.ComponentType<any>>(List: L) {
       }
     }, []);
 
+    const { enabled: parentEnabled } = useContext(ConfigurationContext);
+    const enabled = parentEnabled && $$enabled !== false ? true : false;
+    const enabledRef = useRef(enabled);
+    useEffect(() => {
+      enabledRef.current = enabled;
+    }, [enabled]);
+
     // When self is an item of an observable list.
     useInViewPort(() => {
+      if (!enabled) return;
+
       viewableKeys.forEach((key) => {
         const callbacks = callbacksMap.current?.get(key);
         if (callbacks) {
@@ -170,133 +183,205 @@ export function observe<L extends React.ComponentType<any>>(List: L) {
           }
         });
       };
-    }, []);
+    }, [enabled]);
+
+    useEffect(() => {
+      // true -> false
+      if (!enabled) {
+        // give back all cleans
+        viewableKeys.forEach((key) => {
+          const cleansWithCallback = cleansMap.current?.get(key);
+          cleansWithCallback?.forEach((clean, callback) => {
+            if (typeof clean === 'function') {
+              clean();
+            }
+
+            if (!callbacksMap.current) {
+              callbacksMap.current = new Map();
+            }
+            let callbacks = callbacksMap.current.get(key);
+            if (!callbacks) {
+              callbacks = new Set();
+              callbacksMap.current.set(key, callbacks);
+            }
+            callbacks.add(callback);
+          });
+          cleansMap.current?.delete(key);
+          if (callbacksMap.current?.size === 0) {
+            callbacksMap.current = undefined;
+          }
+        });
+      }
+
+      return () => {
+        // false -> true
+        if (!enabled) {
+          // consume callback
+          viewableKeys.forEach((key) => {
+            const callbacks = callbacksMap.current?.get(key);
+            callbacks?.forEach((callback) => {
+              const inViewPort = isInViewPortRecursively(key);
+              if (inViewPort) {
+                const clean = callback();
+                if (!cleansMap.current) {
+                  cleansMap.current = new Map();
+                }
+                let cleansWithCallback = cleansMap.current.get(key);
+                if (!cleansWithCallback) {
+                  cleansWithCallback = new Map();
+                  cleansMap.current.set(key, cleansWithCallback);
+                }
+                cleansWithCallback.set(callback, clean);
+                callbacks.delete(callback);
+              }
+            });
+            if (callbacks?.size === 0) {
+              callbacksMap.current?.delete(key);
+              if (callbacksMap.current?.size === 0) {
+                callbacksMap.current = undefined;
+              }
+            }
+          });
+        }
+      };
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [enabled]);
 
     const ListComponent = List as any;
 
     return (
-      <CallbacksContext.Provider value={{ addCallback, removeCallback }}>
-        <ListComponent
-          {...props}
-          ref={ref}
-          keyExtractor={keyExtractor}
-          onViewableItemsChanged={useCallback(
-            ({
-              changed,
-              viewableItems,
-            }: {
-              changed: ViewToken[];
-              viewableItems: ViewToken[];
-            }) => {
-              const willHideKeys = new Set(viewableKeys);
+      <ConfigurationContext.Provider value={{ enabled }}>
+        <CallbacksContext.Provider value={{ addCallback, removeCallback }}>
+          <ListComponent
+            {...props}
+            ref={ref}
+            keyExtractor={keyExtractor}
+            onViewableItemsChanged={useCallback(
+              ({
+                changed,
+                viewableItems,
+              }: {
+                changed: ViewToken[];
+                viewableItems: ViewToken[];
+              }) => {
+                const willHideKeys = new Set(viewableKeys);
 
-              for (let i = 0; i < viewableItems.length; i++) {
-                const viewableItem = viewableItems[i];
-                if (!viewableItem) {
-                  continue;
-                }
+                for (let i = 0; i < viewableItems.length; i++) {
+                  const viewableItem = viewableItems[i];
+                  if (!viewableItem) {
+                    continue;
+                  }
 
-                const { item, isViewable } = viewableItem;
+                  const { item, isViewable } = viewableItem;
 
-                if (isViewable) {
-                  const itemKey =
-                    typeof keyExtractor === 'function'
-                      ? keyExtractor(item)
-                      : item;
+                  if (isViewable) {
+                    const itemKey =
+                      typeof keyExtractor === 'function'
+                        ? keyExtractor(item)
+                        : item;
 
-                  const isNew = !viewableKeys.has(itemKey);
-                  if (isNew) {
-                    viewableKeys.add(itemKey);
+                    const isNew = !viewableKeys.has(itemKey);
+                    if (isNew) {
+                      viewableKeys.add(itemKey);
 
-                    const callbacks = callbacksMap.current?.get(itemKey);
-                    callbacks?.forEach((callback) => {
-                      const inViewPort = isInViewPortRecursively(itemKey);
-                      // undefined: This is root observable list.
-                      // true/false: This is inner observable list.
-                      // If FlatList is an inner list, it will notify viewableItems based on itself even if it is outside the viewport of the outer list, so if it is outside the viewport of the outer list (false), execution will be blocked.
-                      if (inViewPort) {
-                        const clean = callback();
-                        if (!cleansMap.current) {
-                          cleansMap.current = new Map();
+                      if (enabled && enabledRef.current) {
+                        const callbacks = callbacksMap.current?.get(itemKey);
+                        callbacks?.forEach((callback) => {
+                          const inViewPort = isInViewPortRecursively(itemKey);
+                          if (inViewPort) {
+                            const clean = callback();
+                            if (!cleansMap.current) {
+                              cleansMap.current = new Map();
+                            }
+                            let cleansWithCallback =
+                              cleansMap.current.get(itemKey);
+                            if (!cleansWithCallback) {
+                              cleansWithCallback = new Map();
+                              cleansMap.current.set(
+                                itemKey,
+                                cleansWithCallback
+                              );
+                            }
+                            cleansWithCallback.set(callback, clean);
+                            callbacks.delete(callback);
+                          }
+                        });
+                        if (callbacks?.size === 0) {
+                          callbacksMap.current?.delete(itemKey);
+                          if (callbacksMap.current?.size === 0) {
+                            callbacksMap.current = undefined;
+                          }
                         }
-                        let cleansWithCallback = cleansMap.current.get(itemKey);
-                        if (!cleansWithCallback) {
-                          cleansWithCallback = new Map();
-                          cleansMap.current.set(itemKey, cleansWithCallback);
-                        }
-                        cleansWithCallback.set(callback, clean);
-                        callbacks.delete(callback);
-                      }
-                    });
-                    if (callbacks?.size === 0) {
-                      callbacksMap.current?.delete(itemKey);
-                      if (callbacksMap.current?.size === 0) {
-                        callbacksMap.current = undefined;
                       }
                     }
-                  }
 
-                  willHideKeys.delete(itemKey);
+                    willHideKeys.delete(itemKey);
+                  }
                 }
-              }
 
-              willHideKeys.forEach((key) => {
-                viewableKeys.delete(key);
+                willHideKeys.forEach((key) => {
+                  viewableKeys.delete(key);
 
-                if (!cleansMap.current) {
-                  cleansMap.current = new Map();
-                }
-                const cleansWithCallback = cleansMap.current.get(key);
+                  if (enabled && enabledRef.current) {
+                    if (!cleansMap.current) {
+                      cleansMap.current = new Map();
+                    }
+                    const cleansWithCallback = cleansMap.current.get(key);
 
-                cleansWithCallback?.forEach((clean, callback) => {
-                  if (typeof clean === 'function') {
-                    clean();
+                    cleansWithCallback?.forEach((clean, callback) => {
+                      if (typeof clean === 'function') {
+                        clean();
+                      }
+
+                      // give back again
+                      if (!callbacksMap.current) {
+                        callbacksMap.current = new Map();
+                      }
+                      let callbacks = callbacksMap.current.get(key);
+                      if (!callbacks) {
+                        callbacks = new Set();
+                        callbacksMap.current.set(key, callbacks);
+                      }
+                      callbacks.add(callback);
+                    });
+                    cleansMap.current.delete(key);
+                    if (cleansMap.current.size === 0) {
+                      cleansMap.current = undefined;
+                    }
                   }
-
-                  // give back again
-                  if (!callbacksMap.current) {
-                    callbacksMap.current = new Map();
-                  }
-                  let callbacks = callbacksMap.current.get(key);
-                  if (!callbacks) {
-                    callbacks = new Set();
-                    callbacksMap.current.set(key, callbacks);
-                  }
-                  callbacks.add(callback);
                 });
-                cleansMap.current.delete(key);
-                if (cleansMap.current.size === 0) {
-                  cleansMap.current = undefined;
-                }
-              });
 
-              onViewableItemsChanged?.({ changed, viewableItems });
-            },
-            [
-              isInViewPortRecursively,
-              keyExtractor,
-              onViewableItemsChanged,
-              viewableKeys,
-            ]
-          )}
-          renderItem={useCallback(
-            (itemProps: any) => {
-              const key = isFunction(keyExtractor)
-                ? keyExtractor(itemProps.item)
-                : itemProps.item;
+                onViewableItemsChanged?.({ changed, viewableItems });
+              },
+              [
+                isInViewPortRecursively,
+                keyExtractor,
+                onViewableItemsChanged,
+                viewableKeys,
+                enabled,
+              ]
+            )}
+            renderItem={useCallback(
+              (itemProps: any) => {
+                const key = isFunction(keyExtractor)
+                  ? keyExtractor(itemProps.item)
+                  : itemProps.item;
 
-              return (
-                <ItemContext.Provider
-                  value={{ key, isInViewPort: isInViewPortRecursively }}
-                >
-                  {renderItem(itemProps)}
-                </ItemContext.Provider>
-              );
-            },
-            [isInViewPortRecursively, keyExtractor, renderItem]
-          )}
-        />
-      </CallbacksContext.Provider>
+                return (
+                  <ItemContext.Provider
+                    value={{ key, isInViewPort: isInViewPortRecursively }}
+                  >
+                    {renderItem(itemProps)}
+                  </ItemContext.Provider>
+                );
+              },
+              [isInViewPortRecursively, keyExtractor, renderItem]
+            )}
+          />
+        </CallbacksContext.Provider>
+      </ConfigurationContext.Provider>
     );
-  }) as unknown as L;
+  }) as unknown as React.ComponentType<
+    React.ComponentProps<L> & { $$enabled?: boolean; ref?: any }
+  >;
 }
