9fc447ee1d04c57de9517edb3ed1a665c452015b
Joondong <kwonjoondong@gmail.com>
2024-10-23 01:30:10 +0900

    fix: expose useIsFirst

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
-import { observe, useInViewPort } from './observe';
+import { observe, useInViewPort, useIsFirst } from './observe';
 
-export { observe, useInViewPort } from './observe';
+export { observe, useInViewPort, useIsFirst } from './observe';
 
 export default {
   observe,
   useInViewPort,
+  useIsFirst,
 };
